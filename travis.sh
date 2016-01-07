bda73cff6ae481c51d8f8a215d5181caf8714be9
Julien HENRY <julien.henry@sonarsource.com>
2016-01-07 14:19:49 +0100

    Change to version 3.0 and enable Dory and P/R analysis

--- travis.sh
+++ travis.sh
@@ -8,24 +8,83 @@ function installTravisTools {
   source ~/.local/bin/install
 }
 
-installTravisTools
+function strongEcho {
+  echo ""
+  echo "================ $1 ================="
+}
 
-if [ -n "${SQ_VERSION:-}" ] && [ "${SQ_VERSION}" == "DEV" ]
-then
-  build_snapshot "SonarSource/sonarqube"
-fi
+case "$TARGET" in
 
-# Need install because mvn org.codehaus.mojo:sonar-maven-plugin:<version>:sonar in ITs will take artifact from local repo
-mvn install -B -e -V
+CI)
+  if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    strongEcho 'Build and analyze commit in master'
+    # this commit is master must be built and analyzed (with upload of report)
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -Dmaven.test.redirectTestOutputToFile=false -B -e -V
 
- 
-if [ "${RUN_ITS}" == "true" ]                                                                                                                                    
-then                                                                                                                                                             
+    # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+    export PATH=$JAVA_HOME/bin:$PATH
 
+    export MAVEN_OPTS="-Xmx1G -Xms128m"
+    mvn sonar:sonar -B -e -V \
+       -Dsonar.host.url=$SONAR_HOST_URL \
+       -Dsonar.login=$SONAR_TOKEN
+
+
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "$GITHUB_TOKEN" ]; then
+    # For security reasons environment variables are not available on the pull requests
+    # coming from outside repositories
+    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests                                                                         
+    # That's why the analysis does not need to be executed if the variable SONAR_GITHUB_OAUTH is not defined.                                                                
+                                                                                                                                                                             
+    strongEcho 'Build and analyze pull request'                                                                                                                              
+    # this pull request must be built and analyzed (without upload of report)                                                                                                
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test -Dmaven.test.redirectTestOutputToFile=false -B -e -V                                   
+                                                                                                                                                                             
+    # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7                                                                                              
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle                                                                                                                              
+    export PATH=$JAVA_HOME/bin:$PATH                                                                                                                                         
+                                                                                                                                                                             
+    mvn sonar:sonar -B -e -V \                                                                                                                                               
+        -Dsonar.analysis.mode=issues \                                                                                                                                       
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_TOKEN
+
+
+  else
+    strongEcho 'Build, no analysis'
+    # Build branch, without any analysis
+
+    # No need for Maven goal "install" as the generated JAR file does not need to be installed
+    # in Maven local repository
+    mvn verify -Dmaven.test.redirectTestOutputToFile=false -B -e -V
+  fi
+  ;;
+
+IT)
+  installTravisTools
+  if [ "${SQ_VERSION}" == "DEV" ]
+  then
+    build_snapshot "SonarSource/sonarqube"
+  fi
+
+  # Need install because mvn org.sonarsource.scanner.maven:sonarqube-maven-plugin:<version>:sonar in ITs will take artifact from local repo
+  mvn verify -B -e -V -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
+  
   maven_home_for_its=${HOME}/maven-for-its
   mkdir -p $maven_home_for_its
   curl -sSL http://repo1.maven.org/maven2/org/apache/maven/apache-maven/$MAVEN_VERSION/apache-maven-$MAVEN_VERSION-bin.tar.gz | tar zx --strip-components 1 -C $maven_home_for_its
 
-  cd its  
-  mvn -Dsonar.runtimeVersion=$SQ_VERSION -Dmaven.test.redirectTestOutputToFile=false -Dmaven.home=$maven_home_for_its verify
-fi
+  cd its 
+  mvn -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false -Dmaven.home=$maven_home_for_its verify
+  ;;
+
+*)
+  echo "Unexpected TARGET value: $TARGET"
+  exit 1
+  ;;
+
+esac

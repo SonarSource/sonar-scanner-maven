15a196475a51ea4809558beb3bd3ea28db259a6f
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-01-20 22:16:10 +0100

    Fix analysis of external PR

--- travis.sh
+++ travis.sh
@@ -16,23 +16,20 @@ function strongEcho {
 case "$TARGET" in
 
 CI)
-  if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     strongEcho 'Build and analyze commit in master'
     # this commit is master must be built and analyzed (with upload of report)
-    mvn -B -e -V clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
        -Pcoverage-per-test \
        -Dmaven.test.redirectTestOutputToFile=false \
        -Dsonar.host.url=$SONAR_HOST_URL \
-       -Dsonar.login=$SONAR_TOKEN
+       -Dsonar.login=$SONAR_TOKEN \
+       -B -e -V 
 
-  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "$GITHUB_TOKEN" ]; then
-    # For security reasons environment variables are not available on the pull requests coming from outside repositories
-    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests                                                                         
-    # That's why the analysis does not need to be executed if the variable GITHUB_TOKEN is not defined.                                                                
-                                                                                                                                                                             
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
     strongEcho 'Build and analyze pull request'                                                                                                                              
     # this pull request must be built and analyzed (without upload of report)                                                                                                
-    mvn -B -e -V clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
         -Pcoverage-per-test \
         -Dmaven.test.redirectTestOutputToFile=false \
         -Dsonar.analysis.mode=issues \
@@ -40,7 +37,8 @@ CI)
         -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
         -Dsonar.github.oauth=$GITHUB_TOKEN \
         -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN
+        -Dsonar.login=$SONAR_TOKEN \
+        -B -e -V 
 
   else
     strongEcho 'Build, no analysis'

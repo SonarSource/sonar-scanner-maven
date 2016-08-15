e801ffc8cee903d64dd6083cc8f5007a09a3eaf8
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-08-15 14:20:06 +0200

    MSONAR-147 Support SONARQUBE_SCANNER_PARAMS and sonar.scanner.skip

--- src/main/java/org/sonarsource/scanner/maven/SonarQubeMojo.java
+++ src/main/java/org/sonarsource/scanner/maven/SonarQubeMojo.java
@@ -40,7 +40,7 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProjectBuilder;
 import org.apache.maven.shared.dependency.tree.DependencyTreeBuilder;
 import org.sonarsource.scanner.api.EmbeddedScanner;
-import org.sonarsource.scanner.maven.bootstrap.EnvSonarQubeScannerParams;
+import org.sonarsource.scanner.api.Utils;
 import org.sonarsource.scanner.maven.bootstrap.LogHandler;
 import org.sonarsource.scanner.maven.bootstrap.MavenProjectConverter;
 import org.sonarsource.scanner.maven.bootstrap.PropertyDecryptor;
@@ -100,7 +100,7 @@ public class SonarQubeMojo extends AbstractMojo {
       return;
     }
     try {
-      Properties envProps = new EnvSonarQubeScannerParams().loadEnvironmentProperties();
+      Properties envProps = Utils.loadEnvironmentProperties(System.getenv());
       ExtensionsFactory extensionsFactory = new ExtensionsFactory(getLog(), session, lifecycleExecutor, artifactFactory, localRepository, artifactMetadataSource, artifactCollector,
         dependencyTreeBuilder, projectBuilder);
       DependencyCollector dependencyCollector = new DependencyCollector(dependencyTreeBuilder, localRepository);

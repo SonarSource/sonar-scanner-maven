17e0e9034237da5a13042ad361a6b42c7e33801d
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-09-13 14:12:28 +0200

    Remove unused import

--- src/test/java/org/sonarsource/scanner/maven/SonarQubeMojoTest.java
+++ src/test/java/org/sonarsource/scanner/maven/SonarQubeMojoTest.java
@@ -30,7 +30,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Properties;
 
 import org.apache.commons.io.IOUtils;

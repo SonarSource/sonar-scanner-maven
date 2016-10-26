a61a8cc637a4906a2f30c81322cccabbc20e8853
David Rautureau <david.rautureau@sonarsource.com>
2016-10-26 14:07:50 +0200

    BUILD-256 Move to the new release process

--- cix.sh
+++ cix.sh
@@ -6,15 +6,14 @@ echo "Running with SQ=$SQ_VERSION for $CI_BUILD_NUMBER"
 
   #deploy the version built by travis
   CURRENT_VERSION=`mvn help:evaluate -Dexpression="project.version" | grep -v '^\[\|Download\w\+\:'`
-  RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
-  NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
-  echo $NEW_VERSION
+
+  echo $CURRENT_VERSION
   mkdir -p target
   cd target
-  curl --user $ARTIFACTORY_QA_READER_USERNAME:$ARTIFACTORY_QA_READER_PASSWORD -sSLO https://repox.sonarsource.com/sonarsource-public-qa/org/sonarsource/scanner/maven/sonar-maven-plugin/$NEW_VERSION/sonar-maven-plugin-$NEW_VERSION.jar
+  curl --user $ARTIFACTORY_QA_READER_USERNAME:$ARTIFACTORY_QA_READER_PASSWORD -sSLO https://repox.sonarsource.com/sonarsource-public-qa/org/sonarsource/scanner/maven/sonar-maven-plugin/$CURRENT_VERSION/sonar-maven-plugin-$CURRENT_VERSION.jar
   cd ..
  
-  mvn install:install-file -Dfile=target/sonar-maven-plugin-$NEW_VERSION.jar
+  mvn install:install-file -Dfile=target/sonar-maven-plugin-$CURRENT_VERSION.jar
 
 
 

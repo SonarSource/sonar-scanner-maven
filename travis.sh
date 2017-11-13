13bc180caa46feb4ff166f8e7f3bd372d1461747
Julien HENRY <julien.henry@sonarsource.com>
2017-11-13 16:47:48 +0100

    Enable QA on P/R + update travis utils

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v39 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
@@ -14,17 +14,6 @@ function strongEcho {
   echo "================ $1 ================="
 }
 
-#build_snapshot "SonarSource/sonar-scanner-api"
+export DEPLOY_PULL_REQUEST=true
 
-case "$TARGET" in
-
-CI)
-  regular_mvn_build_deploy_analyze
-  ;;
-
-*)
-  echo "Unexpected TEST mode: $TARGET"
-  exit 1
-  ;;
-
-esac
+regular_mvn_build_deploy_analyze

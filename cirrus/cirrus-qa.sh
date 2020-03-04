68cf44c93378868e5f0eb83419cb942f573a4121
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2020-03-04 10:34:33 +0100

    fixup

--- cirrus/cirrus-qa.sh
+++ cirrus/cirrus-qa.sh
@@ -4,7 +4,9 @@
 source cirrus-env QA
 source set_maven_build_version $BUILD_NUMBER
 
-# Install a specific version of Maven into $MAVEN_HOME_IT. We want to test against multiple versions  of Maven.
+# Install a specific version of Maven. We want to test against multiple versions.
+MAVEN_HOME_IT=`pwd`/maven_it
+mkdir -p $MAVEN_HOME_IT
 curl -sSL https://repo1.maven.org/maven2/org/apache/maven/apache-maven/$MAVEN_VERSION/apache-maven-$MAVEN_VERSION-bin.tar.gz | tar zx --strip-components 1 -C $MAVEN_HOME_IT
 cp -f ~/.m2/settings.xml $MAVEN_HOME_IT/conf/
 

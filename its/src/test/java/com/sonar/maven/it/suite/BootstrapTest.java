993000d7708a93f738fa84b466e127e0c60485cb
Alban Auzeill <alban.auzeill@sonarsource.com>
2024-10-29 14:52:45 +0100

    SCANMAVEN-237 Test sensor context config, system properties, and environment variables passed to the bootstrapped JRE (#246)

--- its/src/test/java/com/sonar/maven/it/suite/BootstrapTest.java
+++ its/src/test/java/com/sonar/maven/it/suite/BootstrapTest.java
@@ -28,11 +28,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.StringAssert;
 import org.junit.jupiter.api.Test;
 
+import static com.sonar.maven.it.ItUtils.locateProjectDir;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BootstrapTest extends AbstractMavenTest {
+class BootstrapTest extends AbstractMavenTest {
 
   @Test
   void test_unsupported_platform() {
@@ -65,8 +68,35 @@ public class BootstrapTest extends AbstractMavenTest {
     MavenBuild build = MavenBuild.create(ItUtils.locateProjectPom(projectName))
       .setProperty("sonar.login", ORCHESTRATOR.getDefaultAdminToken())
       .setProperty("sonar.host.url", ORCHESTRATOR.getServer().getUrl())
+      .setEnvironmentVariable("DUMP_SENSOR_PROPERTIES", "" +
+        "any.property.name," +
+        "sonar.java.fileByFile," +
+        "sonar.java.file.suffixes," +
+        "sonar.projectKey," +
+        "sonar.projectBaseDir," +
+        "sonar.sources," +
+        "sonar.working.directory," +
+        "sonar.java.libraries," +
+        "sonar.java.source," +
+        "sonar.java.target," +
+        "sonar.java.test.libraries," +
+        "sonar.java.jdkHome")
+      .setEnvironmentVariable("DUMP_ENV_PROPERTIES", "" +
+        "ANY_ENV_VAR")
+      .setEnvironmentVariable("DUMP_SYSTEM_PROPERTIES", "" +
+        "http.proxyUser,"+
+        "http.nonProxyHosts," +
+        "java.home")
+      .setEnvironmentVariable("ANY_ENV_VAR", "42")
+      // http.nonProxyHosts will only be present in the maven context and not in the provisioned JRE
+      .setProperty("http.nonProxyHosts", "localhost|my-custom-non-proxy.server.com")
+      // Any property will be passed to the sensorContext.config()
+      .setProperty("any.property.name", "foo42")
+      // This property will be ignored because of the bellow "sonar.scanner.javaOpts" property that has priority
+      .setEnvironmentVariable("SONAR_SCANNER_JAVA_OPTS", "-Dhttp.proxyUser=my-custom-user-from-env")
+      // Set system property on the provisioned JRE
+      .setProperty("sonar.scanner.javaOpts", "-Dhttp.proxyUser=my-custom-user-from-system-properties")
       .setGoals(cleanSonarGoal());
-
 
     BuildResult result = validateBuildWithCE(runner.runQuietly(null, build));
     assertThat(result.isSuccess()).isTrue();
@@ -74,19 +104,66 @@ public class BootstrapTest extends AbstractMavenTest {
     Properties props = new Properties();
     props.load(Files.newInputStream(propertiesFile));
 
+    Path smallProjectDir = locateProjectDir("maven").getAbsoluteFile().toPath().resolve("bootstrap-small-project");
+
+    SoftAssertions softly = new SoftAssertions();
+
+    // Environment variables
+    softly.assertThat(props.getProperty("ANY_ENV_VAR")).isEqualTo( "42");
+
+    // User defined in its/projects/maven/bootstrap-small-project/pom.xml properties
+    softly.assertThat(props.getProperty("sonar.java.fileByFile")).isEqualTo( "true");
+
+    // SonarQube properties
+    softly.assertThat(props.getProperty("sonar.java.file.suffixes")).isEqualTo( ".java,.jav");
+
+    // Project properties
+    softly.assertThat(props.getProperty("sonar.projectKey")).isEqualTo( "org.sonarsource.maven.its:bootstrap-small-project");
+    softly.assertThat(props.getProperty("sonar.projectBaseDir")).isEqualTo( smallProjectDir.toString());
+    softly.assertThat(props.getProperty("sonar.sources")).isEqualTo( smallProjectDir.resolve("pom.xml") + "," + smallProjectDir.resolve("src").resolve("main").resolve("java"));
+    softly.assertThat(props.getProperty("sonar.working.directory")).isEqualTo( smallProjectDir.resolve("target").resolve("sonar").toString());
+
+    // Any properties are present in the sensor context
+    softly.assertThat(props.getProperty("any.property.name")).contains("foo42");
+
+    // Java analyzers properties
+    softly.assertThat(props.getProperty("sonar.java.libraries")).contains("jsr305-3.0.2.jar");
+    softly.assertThat(props.getProperty("sonar.java.source")).isEqualTo( "11");
+    softly.assertThat(props.getProperty("sonar.java.target")).isEqualTo( "11");
+    softly.assertThat(props.getProperty("sonar.java.test.libraries")).contains("jsr305-3.0.2.jar");
+    // sonar.java.jdkHome should be the one used by "mvn sonar:sonar", by default maven uses JAVA_HOME
+    String javaHome = System.getenv("JAVA_HOME");
+    if (javaHome == null) {
+      javaHome = System.getProperty("java.home");
+    }
+    softly.assertThat(props.getProperty("sonar.java.jdkHome")).isEqualTo( new File(javaHome).getCanonicalPath());
+
+    StringAssert javaHomeAssertion = softly.assertThat(props.getProperty("java.home")).isNotEmpty();
     if (ORCHESTRATOR.getServer().version().isGreaterThanOrEquals(10, 6)) {
       //we test that we are actually using the JRE downloaded from SQ
-      assertThat(props.getProperty("java.home"))
-        .isNotEmpty()
+      javaHomeAssertion
         .isNotEqualTo(System.getProperty("java.home"))
         .contains(".sonar" + File.separator + "cache");
+
+      // System properties of the initial JRE are intentionally not set on the provisioned JRE
+      softly.assertThat(props.getProperty("http.nonProxyHosts"))
+        .isEmpty();
+
+      // System properties defined in "sonar.scanner.javaOpts" are set on the provisioned JRE
+      softly.assertThat(props.getProperty("http.proxyUser")).isEqualTo("my-custom-user-from-system-properties");
     } else {
       //we test that we are using the system JRE
-      assertThat(props.getProperty("java.home"))
-        .isNotEmpty()
+      javaHomeAssertion
         .isEqualTo(System.getProperty("java.home"))
         .doesNotContain(".sonar" + File.separator + "cache");
+
+      softly.assertThat(props.getProperty("http.nonProxyHosts"))
+        .isEqualTo("localhost|my-custom-non-proxy.server.com");
+
+      // System properties defined in "sonar.scanner.javaOpts" are ignored outside the provisioned JRE
+      softly.assertThat(props.getProperty("http.proxyUser")).isEmpty();
     }
+    softly.assertAll();
   }
 
 }

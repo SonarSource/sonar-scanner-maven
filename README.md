2a3998a51abece22bc3c33b99a2997e9853d1586
Alban Auzeill <alban.auzeill@sonarsource.com>
2025-03-12 16:39:30 +0100

    SCANMAVEN-265 Document how to run integration tests locally (#278)

--- README.md
+++ README.md
@@ -10,15 +10,13 @@ For support questions ("How do I?", "I got this error, why?", ...), please head 
 
 Be aware that this forum is a community, so the standard pleasantries ("Hi", "Thanks", ...) are expected. And if you don't get an answer to your thread, you should sit on your hands for at least three days before bumping it. Operators are not standing by. :-)
 
+## Build and execute unit tests and integration tests
+
+* [develop.md](develop.md)
+
 ## Contributing
 
-If you would like to see a new feature, please create a new thread in the forum ["Suggest new features"](https://community.sonarsource.com/c/suggestions/features).
-
-Please be aware that we are not actively looking for feature contributions. The truth is that it's extremely difficult for someone outside SonarSource to comply with our roadmap and expectations. Therefore, we typically only accept minor cosmetic changes and typo fixes.
-
-With that in mind, if you would like to submit a code contribution, please create a pull request for this repository. Please explain your motives to contribute this change: what problem you are trying to fix, what improvement you are trying to make.
-
-Make sure that you follow our [code style](https://github.com/SonarSource/sonar-developer-toolset#code-style) and all tests are passing (Travis build is executed for each pull request).
+* [contributing.md](contributing.md)
 
 ## License
 
--- cirrus/cirrus-qa.sh
+++ cirrus/cirrus-qa.sh
@@ -1,15 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+(
+  REPOSITORY_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd .. && pwd )
+  cd "${REPOSITORY_DIR}"
 
-# Set up environment variables.
-source cirrus-env QA
-source set_maven_build_version $BUILD_NUMBER
+  if [[ "${CIRRUS_CI:-false}" == "true" ]]; then
+    # Set up environment variables.
+    source cirrus-env QA
+    source set_maven_build_version "${BUILD_NUMBER}"
+  else
+    export SQ_VERSION="${SQ_VERSION:-LATEST_RELEASE}"
+    export MAVEN_VERSION="${MAVEN_VERSION:-3.9.4}"
+  fi
 
+  mvn --batch-mode --errors clean
 
-# Install a specific version of Maven. We want to test against multiple versions.
-MAVEN_HOME_IT=`pwd`/maven_it
-mkdir -p $MAVEN_HOME_IT
-curl -sSL https://repo1.maven.org/maven2/org/apache/maven/apache-maven/$MAVEN_VERSION/apache-maven-$MAVEN_VERSION-bin.tar.gz | tar zx --strip-components 1 -C $MAVEN_HOME_IT
-cp -f ~/.m2/settings.xml $MAVEN_HOME_IT/conf/
+  # Install a specific version of Maven. We want to test against multiple versions.
+  MAVEN_HOME_IT="${REPOSITORY_DIR}/target/downloaded-maven-${MAVEN_VERSION}"
+  mkdir -p "${MAVEN_HOME_IT}"
 
-# we don't need to rebuild the sonar-maven-plugin, it is already deployed on repox
-mvn --batch-mode --errors --projects '!sonar-maven-plugin' --activate-profiles its -Dsonar.runtimeVersion=$SQ_VERSION -Dmaven.test.redirectTestOutputToFile=false -Dmaven.home=$MAVEN_HOME_IT verify
+  MAVEN_BINARY_URL="https://repo1.maven.org/maven2/org/apache/maven/apache-maven/${MAVEN_VERSION}/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+  echo "Extracting apache-maven-${MAVEN_VERSION}-bin.tar.gz into ${MAVEN_HOME_IT}"
+  curl -sSL "${MAVEN_BINARY_URL}" | tar zx --strip-components 1 -C "${MAVEN_HOME_IT}"
+  cp -f "${HOME}/.m2/settings.xml" "${MAVEN_HOME_IT}/conf/"
+
+  mvn --batch-mode --errors \
+    --projects '!sonar-maven-plugin' \
+    --activate-profiles its \
+    -Dsonar.runtimeVersion="${SQ_VERSION}" \
+    -Dmaven.home="${MAVEN_HOME_IT}" \
+    -Dmaven.test.redirectTestOutputToFile=false \
+    verify
+)

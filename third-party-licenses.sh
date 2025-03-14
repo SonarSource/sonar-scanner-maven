7f429182293e630cae34a14e8d089648b8348761
erwan-serandour <erwan.serandour@sonarsource.com>
2025-03-14 15:41:30 +0100

    SCANMAVEN-277 remove unused third-party-licenses.sh (#287)

--- third-party-licenses.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ./sonar-maven-plugin
-mvn org.codehaus.mojo:license-maven-plugin:aggregate-add-third-party -Dlicense.includedScopes=compile
-
-cat target/generated-sources/license/THIRD-PARTY.txt

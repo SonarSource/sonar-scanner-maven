bde882cbf1fe6b94aea4113f5c848bab3c449ddd
Rudy Regazzoni <rudy.regazzoni@sonarsource.com>
2025-08-12 08:48:23 +0200

    SCANMAVEN-296 Include github actions folder in the scan

--- sonar-maven-plugin/src/test/java/org/sonarsource/scanner/maven/bootstrap/MavenProjectConverterTest.java
+++ sonar-maven-plugin/src/test/java/org/sonarsource/scanner/maven/bootstrap/MavenProjectConverterTest.java
@@ -698,6 +698,23 @@ class MavenProjectConverterTest {
       .containsOnly(originalPom.getAbsolutePath());
   }
 
+  @Test
+  void shouldIncludeGithubActionFolder() throws Exception {
+    File baseDir = temp.toAbsolutePath().toFile();
+    File githubDir = new File(baseDir, ".github");
+    githubDir.mkdirs();
+    MavenProject project = createProject(new Properties(), "jar");
+
+    Map<String, String> props = projectConverter.configure(Collections.singletonList(project), project, new Properties());
+    assertThat(props).containsEntry("sonar.projectKey", "com.foo:myProject")
+      .containsEntry("sonar.projectName", "My Project")
+      .containsEntry("sonar.projectVersion", "2.1");
+    assertThat(props.get("sonar.sources").split(",")).containsExactly(
+      new File(baseDir, "pom.xml").getAbsolutePath(),
+      githubDir.getAbsolutePath()
+    );
+  }
+
   private MavenProject createProject(Properties pomProps, String packaging) throws IOException {
     File pom = temp.resolve("pom.xml").toFile();
     pom.createNewFile();
